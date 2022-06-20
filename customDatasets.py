69fb16e7b213967fe6fe30f245274737f5ad5368
abcrohit <66174896+abcrohit@users.noreply.github.com>
2022-06-20 16:44:01 +0530

    Create customDatasets.py
    helper to upload to studi repo

---
+++ customDatasets.py
@@ -0,0 +1,31 @@
+from torch.utils.data import Dataset
+
+class MyDataset(Dataset):
+  def __init__(self,df):
+    x=df.iloc[:,0:-1].values
+    y=df.iloc[:,-1].values.tolist()
+    y=[int(i) for i in y]
+ 
+    self.x_train=torch.tensor(x,dtype=torch.float32)
+    self.y_train=torch.tensor(y,dtype=torch.float32)
+ 
+  def __len__(self):
+    return len(self.y_train)
+
+  def __getitem__(self,idx):
+    return self.x_train[idx],self.y_train[idx]
+	
+class MyDatasetCompress(Dataset):
+  def __init__(self,df):
+    x=transform(df.iloc[:,:-1].values)
+    y=df.iloc[:,-1].values.tolist()
+    y=[int(i) for i in y]
+ 
+    self.x_train=torch.tensor(x,dtype=torch.float32)
+    self.y_train=torch.tensor(y,dtype=torch.float32)
+ 
+  def __len__(self):
+    return len(self.y_train)
+
+  def __getitem__(self,idx):
+    return self.x_train[idx],self.y_train[idx]
